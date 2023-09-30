6e018aa9210801a1a25ef73ddbf6973483f44daa
srikrishnanaik <srikrishnanaikb@gmail.com>
2023-10-01 01:37:50 +0530

    Rust language

---
+++ hello-rust/target/debug/deps/hello_rust.d
@@ -0,0 +1,5 @@
+D:\Rust\Solution1\hello-rust\target\debug\deps\hello_rust.exe: src\main.rs
+
+D:\Rust\Solution1\hello-rust\target\debug\deps\hello_rust.d: src\main.rs
+
+src\main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+D:\Rust\Solution1\hello-rust\target\debug\hello-rust.exe: D:\Rust\Solution1\hello-rust\src\main.rs
